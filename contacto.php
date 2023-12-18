2ae9264fe8329d3d41289cf40f2c33247390c6e9
Pablo Lires <pablolires@gmail.com>
2023-12-18 17:19:29 -0300

    modificacion de direccion en footer y contacto

--- contacto.php
+++ contacto.php
@@ -8,114 +8,116 @@
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-	<!-- Tag Manager Head -->
-	<?php include_once("./includes/tag_manager_head.php"); ?>
-	
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="El paso más importante de un proyecto Es el primero: ADELANTE">
-	<title>VQZ - Constructora - Contacto</title>
+  <!-- Tag Manager Head -->
+  <?php include_once("./includes/tag_manager_head.php"); ?>
 
-	<!-- Favicons -->
-	<?php include('includes/favicon.php'); ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="El paso más importante de un proyecto Es el primero: ADELANTE">
+  <title>VQZ - Constructora - Contacto</title>
 
-	<link rel="stylesheet" type="text/css" href="./node_modules/normalize.css/normalize.css">
-	<link rel="stylesheet" type="text/css" href="./fontawesome/css/all.min.css">
-	<link rel="stylesheet" type="text/css" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="./node_modules/aos/dist/aos.css"/>
-	<link rel="stylesheet" type="text/css" href="./css/app.css">
+  <!-- Favicons -->
+  <?php include('includes/favicon.php'); ?>
+
+  <link rel="stylesheet" type="text/css" href="./node_modules/normalize.css/normalize.css">
+  <link rel="stylesheet" type="text/css" href="./fontawesome/css/all.min.css">
+  <link rel="stylesheet" type="text/css" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="./node_modules/aos/dist/aos.css" />
+  <link rel="stylesheet" type="text/css" href="./css/app.css">
 </head>
+
 <body>
-	<!-- Tag Manager Body -->
-	<?php include_once("./includes/tag_manager_body.php"); ?>
+  <!-- Tag Manager Body -->
+  <?php include_once("./includes/tag_manager_body.php"); ?>
 
-	<!-- Contenido CONTACTO -->
-	<section id="app" class="obras page_contacto">
+  <!-- Contenido CONTACTO -->
+  <section id="app" class="obras page_contacto">
 
-		<!-- Header -->
-		<?php include_once('./includes/header.php'); ?>
+    <!-- Header -->
+    <?php include_once('./includes/header.php'); ?>
 
-		<!-- Informacion -->
-		<section class="first_section container informacion">
-		  <div class="row">
-		  	<div class="col-md-6 offset-md-3">
-		  		<h1 data-aos="fade-right" class="bebas">CONTÁCTANOS</h1>
-		  	</div>
-		  </div>
-		</section>
-		<!-- Informacion end -->
+    <!-- Informacion -->
+    <section class="first_section container informacion">
+      <div class="row">
+        <div class="col-md-6 offset-md-3">
+          <h1 data-aos="fade-right" class="bebas">CONTÁCTANOS</h1>
+        </div>
+      </div>
+    </section>
+    <!-- Informacion end -->
 
-		<!-- Data -->
-		<section class="container data">
-		  <div class="row">
-		  	<div data-aos="fade-up" class="col-md-12 content_data">
-		  		<h2 class="bebas">Ubicación</h2>
-		  		<div class="content_data_whit_bandera">
-			  		<div class="content_banderas">
-		  				<img class="bandera" src="./img/home/icono-argentina-color.png" alt="bandera argentina">
-		  			</div>
-		  			<p>Av. Donato Álvarez 224 Piso 9 "A" (C1406BON). CABA - ARGENTINA</p>
-		  		</div>
-		  	</div>
-		  	<div data-aos="fade-up" class="col-md-12 content_data">
-		  		<h2 class="bebas">TelÉfono</h2>
-		  		<div class="content_data_whit_bandera">
-			  		<div class="content_banderas">
-		  				<img class="bandera" src="./img/home/icono-argentina-color.png" alt="bandera argentina tel">
-		  			</div>
-		  			<p>(+54 11) 4633 - 1200</p>
-		  		</div>
-		  	</div>
-		  	<div data-aos="fade-up" class="col-md-12 content_data">
-		  		<h2 class="bebas">email</h2>
-		  		<div class="content_data_whit_bandera">
-			  		<div class="content_banderas">
-		  				<img class="bandera" src="./img/home/icono-argentina-color.png" alt="bandera argentina tel">
-			  			<img class="bandera" src="./img/home/icono-espana-color.png" alt="bandera espana mail">
-		  			</div>
-		  			<p>info@vazquez.net</p>
-		  		</div>
-		  	</div>
-		  </div>
-		</section>
-		<!-- Data end -->
+    <!-- Data -->
+    <section class="container data">
+      <div class="row">
+        <div data-aos="fade-up" class="col-md-12 content_data">
+          <h2 class="bebas">Ubicación</h2>
+          <div class="content_data_whit_bandera">
+            <div class="content_banderas">
+              <img class="bandera" src="./img/home/icono-argentina-color.png" alt="bandera argentina">
+            </div>
+            <p>Av Tte Gral Donato Alvarez 221 Oficina 105 (C1406BOA). CABA - ARGENTINA</p>
+          </div>
+        </div>
+        <div data-aos="fade-up" class="col-md-12 content_data">
+          <h2 class="bebas">TelÉfono</h2>
+          <div class="content_data_whit_bandera">
+            <div class="content_banderas">
+              <img class="bandera" src="./img/home/icono-argentina-color.png" alt="bandera argentina tel">
+            </div>
+            <p>(+54 11) 4633 - 1200</p>
+          </div>
+        </div>
+        <div data-aos="fade-up" class="col-md-12 content_data">
+          <h2 class="bebas">email</h2>
+          <div class="content_data_whit_bandera">
+            <div class="content_banderas">
+              <img class="bandera" src="./img/home/icono-argentina-color.png" alt="bandera argentina tel">
+              <img class="bandera" src="./img/home/icono-espana-color.png" alt="bandera espana mail">
+            </div>
+            <p>info@vazquez.net</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Data end -->
 
-		<!-- Formulario -->
-		<section class="container-fluid contacto section_form">
-			<div class="container">
-			  <div class="row">
+    <!-- Formulario -->
+    <section class="container-fluid contacto section_form">
+      <div class="container">
+        <div class="row">
 
-			  	<div data-aos="fade-up" class="col-md-8 offset-md-2 col-lg-6 offset-lg-3 content_title">
-			  		<h2>¿Tenés una consulta? <br><span>Escribínos</span></h2>
-			  	</div>
+          <div data-aos="fade-up" class="col-md-8 offset-md-2 col-lg-6 offset-lg-3 content_title">
+            <h2>¿Tenés una consulta? <br><span>Escribínos</span></h2>
+          </div>
 
-			  	<div data-aos="fade-up" class="col-md-8 offset-md-2 col-lg-6 offset-lg-3">
+          <div data-aos="fade-up" class="col-md-8 offset-md-2 col-lg-6 offset-lg-3">
 
-			  		<!-- Mensages -->
-						<?php include_once('./includes/msg.php'); ?>
+            <!-- Mensages -->
+            <?php include_once('./includes/msg.php'); ?>
 
-						<!-- Errors -->
-						<?php include_once('./includes/errors.php'); ?>
+            <!-- Errors -->
+            <?php include_once('./includes/errors.php'); ?>
 
-						<!-- Formulario -->
-						<?php include_once('./includes/parts/formulario.php'); ?>
+            <!-- Formulario -->
+            <?php include_once('./includes/parts/formulario.php'); ?>
 
-			  	</div>
+          </div>
 
-			  </div>
-			</div>
-		</section>
-		<!-- Formulario end -->
+        </div>
+      </div>
+    </section>
+    <!-- Formulario end -->
 
-	</section>
-	<!-- Contenido CONTACTO end -->
+  </section>
+  <!-- Contenido CONTACTO end -->
 
-	<!-- Footer -->
-	<?php include_once('./includes/footer.php'); ?>
+  <!-- Footer -->
+  <?php include_once('./includes/footer.php'); ?>
 
-	<script src="https://www.google.com/recaptcha/api.js?render=<?= $_ENV['RECAPTCHA_KEY_SITE'] ?>"></script>
-	<script src="./dist/bundle.js"></script>
+  <script src="https://www.google.com/recaptcha/api.js?render=<?= $_ENV['RECAPTCHA_KEY_SITE'] ?>"></script>
+  <script src="./dist/bundle.js"></script>
 
 </body>
 
--- includes/footer.php
+++ includes/footer.php
@@ -1,46 +1,47 @@
 <footer class="container-fluid">
-	<div class="container">
-		<div class="row">
+  <div class="container">
+    <div class="row">
 
-			<div class="col-md-4 col-lg-3 logo_footer">
-				<img class="img-fluid" src="./img/footer/logo-vazquez-35.gif" alt="logo vqz footer">
-			</div>
+      <div class="col-md-4 col-lg-3 logo_footer">
+        <img class="img-fluid" src="./img/footer/logo-vazquez-35.gif" alt="logo vqz footer">
+      </div>
 
-			<div class="col-md-8 col-lg-6 content_links">
-				<ul>
-					<li><a class="transition" href="./">INICIO</a></li>
-					<li><a class="transition" href="./empresa.php">EMPRESA</a></li>
-					<li><a class="transition" href="./obras-realizadas.php">OBRAS REALIZADAS</a></li>
-					<li><a class="transition" href="./contacto.php">CONTACTO</a></li>
-				</ul>
-			</div>
+      <div class="col-md-8 col-lg-6 content_links">
+        <ul>
+          <li><a class="transition" href="./">INICIO</a></li>
+          <li><a class="transition" href="./empresa.php">EMPRESA</a></li>
+          <li><a class="transition" href="./obras-realizadas.php">OBRAS REALIZADAS</a></li>
+          <li><a class="transition" href="./contacto.php">CONTACTO</a></li>
+        </ul>
+      </div>
 
-			<div class="col-md-12 col-lg-3 content_data">
-				<ul>
-					<li>
-						<i class="fa-solid fa-mobile-screen"></i>
-						<a class="transition" target="_blank" rel="noopener" href="tel:541146331200">+54 11 4633-1200</a>
-					</li>
-					<li>
-						<i class="fa-regular fa-envelope"></i>
-						<a class="transition" target="_blank" rel="noopener" href="mailto:info@vazquez.net">info@vazquez.net</a>
-					</li>
-					<li>
-						<i class="fa-solid fa-location-dot"></i>
-						<a class="transition" target="_blank" rel="noopener" href="https://goo.gl/maps/dkEZx2hhYojHhV257">Av. Donato Álvarez 224 P9 "A"</a>
-					</li>
-				</ul>
-			</div>
+      <div class="col-md-12 col-lg-3 content_data">
+        <ul>
+          <li>
+            <i class="fa-solid fa-mobile-screen"></i>
+            <a class="transition" target="_blank" rel="noopener" href="tel:541146331200">+54 11 4633-1200</a>
+          </li>
+          <li>
+            <i class="fa-regular fa-envelope"></i>
+            <a class="transition" target="_blank" rel="noopener" href="mailto:info@vazquez.net">info@vazquez.net</a>
+          </li>
+          <li>
+            <i class="fa-solid fa-location-dot"></i>
+            <a class="transition" target="_blank" rel="noopener" href="https://maps.app.goo.gl/eQHRCE158dDidXEY9">Av.
+              Tte Gral Donato Alvarez 221 Oficina 105 (C1406BOA)</a>
+          </li>
+        </ul>
+      </div>
 
-		</div>
+    </div>
 
-		<div class="row">
-			<div class="col-md-12 content_frase">
-				<p><?= date('Y') ?> Vazquez Obras & Servicios, todos los derechos reservados.</p>
-			</div>
-			<div class="col-md-12 libre">
-				<a class="transition" target="_blank" rel="noopener" href="https://librecomunicacion.net/">by Libre</a>
-			</div>
-		</div>
-	</div>
+    <div class="row">
+      <div class="col-md-12 content_frase">
+        <p><?= date('Y') ?> Vazquez Obras & Servicios, todos los derechos reservados.</p>
+      </div>
+      <div class="col-md-12 libre">
+        <a class="transition" target="_blank" rel="noopener" href="https://librecomunicacion.net/">by Libre</a>
+      </div>
+    </div>
+  </div>
 </footer>
